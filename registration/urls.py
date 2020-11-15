b7bc29e4da2cb2eb02f569b0d9125db701e48480
97Dmitry <dart_veyder_1997@mail.ru>
2020-11-15 19:17:15 +0600

    Was changed interface, added during registration can specify email, added authorization page

--- registration/urls.py
+++ registration/urls.py
@@ -3,6 +3,7 @@ from registration import views
 
 urlpatterns = [
     url('sing_up/', views.sing_up, name='sing_up'),
+    url('sing_in/', views.sing_in, name='sing_in'),
 
 ]
 
--- registration/views.py
+++ registration/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SingUp
 
 
 # Create your views here.
 
 def sing_up(request):
+    form = SingUp()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SingUp(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
+#            username = form.cleaned_data.get('username')
+#            raw_password = form.cleaned_data.get('password')
+#            user = authenticate(username=username, password=raw_password)
+#            login(request, user)
             return redirect('post_list')
     else:
-        form = UserCreationForm()
-    return render(request, 'registration/sing_up.html', {'form': form})
+        form = SingUp()
+    context = {'form': form}
+    return render(request, 'registration/sing_up.html', context)
+
+def sing_in(request):
+    if request.method == 'POST':
+        None
+
+
+    return render(request, 'authorization/sing_in.html')
+
+
