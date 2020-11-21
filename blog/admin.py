7157986923f9ca011bd0d5695e601215997daa0d
97Dmitry <dart_veyder_1997@mail.ru>
2020-11-21 15:54:42 +0600

    Improved admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('title', 'text', 'author', 'published_date')
+	list_display_links = ('title', 'text')
+	search_fields = ('author', 'title', 'text')
+
+admin.site.register(Post, PostAdmin)
