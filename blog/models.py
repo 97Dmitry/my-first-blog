4ecb878bfdb9fcc83dd4f29405e88469b7a086ac
97Dmitry <dart_veyder_1997@mail.ru>
2020-12-01 01:22:00 +0600

    PEP-8 fixes

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.core import validators
 
+
 # Create your models here.
 
 class Post(models.Model):
@@ -23,17 +24,16 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title # Из БД будет возвращаться тайтл экземпляра в виде строки
+        return self.title  # Из БД будет возвращаться тайтл экземпляра в виде строки
 
 
 class Rubric(models.Model):
-
-    rubric = models.CharField(max_length= 25, db_index=True, verbose_name='Тег')
+    rubric = models.CharField(max_length=25, db_index=True, verbose_name='Тег')
 
     class Meta:
         verbose_name_plural = 'Теги'
         verbose_name = 'Тег'
         ordering = ['rubric']
 
-    def __str__(self):      # Указывает на то, что все теги
+    def __str__(self):  # Указывает на то, что все теги
         return self.rubric  # будут называется своим именем
