968f52d175a99666299dba6f12424e0dfd889996
97Dmitry <dart_veyder_1997@mail.ru>
2020-12-08 16:08:51 +0600

    Added post_save signal for create new usres for model Profile

--- test_site/settings.py
+++ test_site/settings.py
@@ -34,8 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
-    'users',
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 
 ]
 
--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    picture = models.ImageField(default='user_pic\\def.jpg', upload_to='user_pic')
+    picture = models.ImageField(default='def.jpg', upload_to='user_pic')
 
     def __str__(self):
         return f'{self.user.username}'
