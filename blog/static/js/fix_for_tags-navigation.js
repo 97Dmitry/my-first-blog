4cee3df262931d7f964d949df359705131b0ee46
97Dmitry <dart_veyder_1997@mail.ru>
2021-01-09 04:43:37 +0600

    Added chat link and now navigation bar is fixed on scrolling

--- blog/static/js/fix_for_tags-navigation.js
+++ blog/static/js/fix_for_tags-navigation.js
@@ -1,2 +1,19 @@
 const div = document.querySelector('.container--content')
 div.style.marginLeft = '5px'
+
+$(function(){
+	$div = $('.container--content')
+	$nav = $('.container_nav');
+	$nav.css('width', $nav.outerWidth());
+	$window = $(window);
+	$h = $nav.offset().top;
+	$window.scroll(function(){
+		if ($window.scrollTop() > $h) {
+			$nav.addClass('fixed');
+			$div.addClass('Left')
+		} else {
+			$nav.removeClass('fixed');
+			$div.removeClass('Left')
+		}
+	});
+});
