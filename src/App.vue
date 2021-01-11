a2d570fdebd8ec3d648fbcce7012d1b58e32f4b1
97Dmitry <dart_veyder_1997@mail.ru>
2021-01-12 02:56:19 +0600

    Has been added Vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>VUE</h1>
+    <hr>
+    <br />
+    <p>This page has been created for ToDo: learn  </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
