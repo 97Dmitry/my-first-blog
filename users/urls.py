3919df57e680008f4a733ee18d62644ae4d66dcd
97Dmitry <dart_veyder_1997@mail.ru>
2021-03-19 19:06:48 +0600

    Fixed link's password recovery

--- users/urls.py
+++ users/urls.py
@@ -15,8 +15,10 @@ urlpatterns = [
     path('user_page/', views.user_page, name='user_page'),
     path('recovery_password/', views.PasswordRecovery.as_view(), name='recovery_password'),
     path('password_recovery_done', views.PasswordRecoveryDone.as_view(), name='password_recovery_done'),
-    path('recovery_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    path('recovery_confirm/<uidb64>/<token>/',
          views.PasswordRecoveryConfirm.as_view(), name='recovery_confirm'),
+    # path('recovery_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    #      views.PasswordRecoveryConfirm.as_view(), name='recovery_confirm'),
     path('recovery_complete/', views.PasswordRecoverComplete.as_view(), name='recovery_complete'),
 
     # For REST need use 'api/...'
