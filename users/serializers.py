2c60d62a079e83e36de8228cd2ed90cba23a62c4
97Dmitry <dart_veyder_1997@mail.ru>
2021-03-31 19:19:49 +0600

    Added djoser and jwt for auth communication with frontend

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,3 +10,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ("user", "picture", "user_rank")
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ("username", "email",)
